1edab4cebbf5cde09938a01171926ccb59c320af
everett980 <everett980@gmail.com>
2016-05-07 18:20:14 -0400

    stores uid after login or register

--- src/containers/UserLogin/UserLogin.js
+++ src/containers/UserLogin/UserLogin.js
@@ -2,19 +2,23 @@ import React, {Component} from 'react';
 import Helmet from 'react-helmet';
 import { MiniInfoBar } from 'components';
 import Firebase from 'firebase';
+import { signin, signout } from '../../redux/modules/setLogin';
+import { connect } from 'react-redux';
 
 const ref = new Firebase('https://crackling-inferno-5205.firebaseio.com/');
 
-export default class UserLogin extends Component {
+class UserLogin extends Component {
 	signUp() {
 		ref.createUser({
 			email	 : this.state.email,
 			password : this.state.password
 		}, function(error, userData) {
 			console.log(arguments);
+			that.props.signin(authData.uid);
 		});
 	}
 	login() {
+		var that = this;
 		ref.authWithPassword({
 			email    : this.state.email,
 			password : this.state.password 
@@ -23,15 +27,16 @@ export default class UserLogin extends Component {
 				console.log("Login Failed!", error);
 			} else {
 				console.log("Authenticated successfully with payload:", authData);
+				that.props.signin(authData.uid);
 			}
 		});
 	}
 	/* getInitialState() {
-		return {
-			email: "",
-			password: ""
-		}
-	} */
+	   return {
+	   email: "",
+	   password: ""
+	   }
+	   } */
 	setEmail(e) {
 		console.log(e.target.value);
 		this.setState({email: e.target.value});
@@ -39,6 +44,9 @@ export default class UserLogin extends Component {
 	setPassword(e) {
 		console.log(e.target.value);
 		this.setState({password: e.target.value});
+	}
+	signOut() {
+		this.props.signout();
 	}
 	test() {
 		console.log(this.state);
@@ -49,6 +57,7 @@ export default class UserLogin extends Component {
 				<h1>UserLogin</h1>
 				<Helmet title="UserLogin Us"/>
 				<br/>	
+				<div style={this.props.uid ? { display: 'none' } : {}}>
 				Email: <input
 				type="text"
 				onChange={::this.setEmail}
@@ -62,6 +71,29 @@ export default class UserLogin extends Component {
 				<button onClick={::this.signUp}>Sign Up</button>
 				<button onClick={::this.login}>Login</button>
 				</div>
+				<div style={!this.props.uid ? { display: 'none' } : {}}>
+				<button onClick={::this.signOut}>Log Out</button>
+				</div>
+				</div>
 			   );
 	}
 }
+
+function mapStateToProps(state) {
+	return {
+		uid: state.setLogin.uid
+	}
+}
+
+function mapDispatchToProps(dispatch) {
+	return {
+		signin: (uid) => {
+					dispatch(signin(uid));
+				},
+		signout: () => {
+					 dispatch(signout());
+				 }
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserLogin);
--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -8,6 +8,7 @@ import counter from './counter';
 import {reducer as form} from 'redux-form';
 import info from './info';
 import widgets from './widgets';
+import setLogin from './setLogin';
 
 export default combineReducers({
   routing: routerReducer,
@@ -20,5 +21,6 @@ export default combineReducers({
     counter3: counter
   }),
   info,
-  widgets
+  widgets,
+  setLogin
 });
